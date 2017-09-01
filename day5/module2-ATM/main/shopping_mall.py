1aa7103ebc3ae53ad4546c6932a6a0972624d7df
zww <ddzhiwenwei@163.com>
2017-09-01 18:47:35 +0800

    2017-09-01

--- day5/module2-ATM/main/shopping_mall.py
+++ day5/module2-ATM/main/shopping_mall.py
@@ -10,11 +10,26 @@ shopping_car = base_dir + '/db/shopping_car'
 creditcard_data = base_dir + '/db/creditcard_data'
 
 '''购物商城'''
+
 def mall():
-    print("欢迎来到购物商城".center(30,'-'))
     product_list = []
+    product_list2 = []
     with open(shopping_data,'r',encoding='utf8') as f:
         for i in f:
             product_list.append(i.strip("\n").split(' ')) #拆分为列表并添加到product_list
-    print(product_list)
+    #print(product_list,len(product_list))
+    def product_info():
+        for index,item in enumerate(product_list):
+            print(index +1,item[0],item[1])
+        #product_info()
+    while True:
+        print("欢迎来到购物商城".center(30, '-'))
+        product_info()
+        choice_id = input("请输入商品编号：").strip()
+        if choice_id.isdigit():
+            choice_id = int(choice_id)
+            if 0 <= choice_id <= len(product_list):
+
+    else:
+        print("没有对应的商品编号，请重新输入！")
 mall()
