6c16f6c586d0f149cf70bec7607e418b96090636
zww <ddzhiwenwei@163.com>
2017-09-22 18:51:53 +0800

    09-22company

--- kaohe1.py
+++ kaohe1.py
@@ -1,27 +1,40 @@
 #coding:utf-8
 #Author:Mr Zhi
-"""
-1.#
-11. 有如下变量，请实现要求的功能
-        tu = ("alex", [11, 22, {"k1": 'v1', "k2": ["age", "name"], "k3": (11,22,33)}, 44])
+import copy
+# a = "i love python"
+# b = a
+# a1 = copy.copy(a)
+# a2 = copy.deepcopy(b)
+# print(id(a))
+# print(id(b))
+# print(id(a1))
+# print(id(a2))
 
-        a. 讲述元祖的特性
-          元组也叫只读列表，跟列表差不多，它只有两个方法，一个是count，一个是index
-
-        b. 请问tu变量中的第一个元素 “alex” 是否可被修改？
-            元组不可直接被修改，需要转换成列表或字典
-
-        c. 请问tu变量中的"k2"对应的值是什么类型？是否可以被修改？如果可以，请在其中添加一个元素 “Seven”
-             k2是字典的键，对应的值是列表可修改：tu[1][2]['k2']='Seven'
-        d. 请问tu变量中的"k3"对应的值是什么类型？是否可以被修改？如果可以，请在其中添加一个元素 “Seven”
-            k3是字典的键，对应的值是列表不可修改
-"""
-total = 0
-i = 0
-while (i < 100):
-    i +=1
-    if i % 2==0:
-        total -=i
-    else:
-        total +=i
-print(total)
+# D = {'k1':'v1','k2':123,'k3':["str1",469]}
+# D2 = copy.copy(D)  #浅拷贝，额外创建第一层（变量名id改变，对象id不变，还是引用旧对象）
+# print(id(D))
+# print(id(D2))
+# print(id(D["k1"]))
+# print(id(D2["k1"]))
+# D = {'k1':'v1','k2':123,'k3':["str1",469]}
+# D["k3"][0] = "gaibian"
+# D["k1"] = "value"
+# D2 = copy.deepcopy(D)
+# print(id(D))
+# print(id(D2))
+# print(id(D["k3"]))
+# print(id(D2["k3"]))
+# print(id(D["k2"]))
+# print(id(D2["k2"]))
+# print(D)
+# print(D2)
+# dict = {"cpu":[100],"mem":[200],"disk":[300]}
+# new_dict = copy.deepcopy(dict)
+# new_dict["cpu"][0] = 90
+# print(new_dict)
+# print(dict)
+# import sys
+# sys.path.append("D:\\软件\\python\\day5")
+# print(sys.path)
+import re
+re.match().group()
--- 考核.py
+++ 考核.py
@@ -1,22 +1,59 @@
-#coding:utf-8
-#Author:Mr Zhi
-#使用while循环实现输出1 - 2 + 3 - 4 + 5 ... - 100 的和
-#偶数和减去奇数和就可以得出答案
-i = 0
-l1 = [] #所有偶数加到这个列表
-l2 = [] #所有奇数加到这个列表
-while (i < 100):
-    i += 1
-    if i % 2 == 0:
-        l1.append(i)
-    if i % 2 == 1:
-        l2.append(i)
-sum1 = 0
-sum2 = 0
-for b in l1:
-    sum1 += b
-print(sum)
-for c in l2:
-    sum2 += c
-sum = sum1 - sum2 #偶数和减去奇数和
-print(sum)
+# #coding:utf-8
+# #Author:Mr Zhi
+# #使用while循环实现输出1 - 2 + 3 - 4 + 5 ... - 100 的和
+# #偶数和减去奇数和就可以得出答案
+# #coding:utf-8
+# # from json import dumps
+# # import json
+# # data = {'name':'JieSen','height':175,'weight':'68KG'}
+# # #dumps到字串
+# # json_str = json.dumps(data)
+# # print('dumps到字符串:',json_str,'类型:',type(json_str))
+# # #loads回来
+# # json_dict = json.loads(json_str)
+# # print('loads回来:',json_dict,'类型:',type(json_dict))
+# # import datetime,json
+# # print(json.dumps(str(datetime.datetime.now())))
+import datetime
+def func_name(is_show=True):#定义一个带有参数的装饰函数
+    def wrap(func):#包装函数，接受一个函数对象作为参数
+        def inner_wrap(*args,**kwargs):
+            if is_show:
+                print("Function name:%s"%(func.__name__))
+            return func(*args,**kwargs)
+        return inner_wrap
+    return wrap
+@func_name(True)      #等于 func_time = func_name(func_time)
+def func_time1():
+    print(datetime.datetime.now())
+@func_name(False)
+def func_time2():
+    print(datetime.datetime.now())
+func_time1()
+func_time2()
+#
+# 总结
+# 1.定义一个装饰器函数，此函数会接受函数对象作为输入参数，以确保能执行其功能
+# 2.在装饰器函数内定义一个和目标函数参数列表一致的包装函数，返回值（包装函数），同时添加欲追加的工作量（甚至彻底替换掉目标函数）
+# 3.装饰器函数返回值设置为包装函数
+# 4.把目标函数对象传递给装饰器函数去执行，返回值（包装函数）赋值到目标函数名上，最后以目标函数之名调用包装函数
+# from datetime import datetime as dt
+# def log(is_show=True):
+#     def wrapper(func):
+#         def inner_wrapper(*args, **kwargs):
+#             if is_show:
+#                 print('['+str(dt.now())+']'+func.__name__)
+#             return func(*args, **kwargs)
+#         return inner_wrapper
+#     return wrapper
+# # 默认显示日志
+# @log(True)
+# def func1():
+#     print("func1")
+# # 默认不显示日志
+# @log(False)
+# def func2():
+#     print("func2")
+# func2()
+# func1()
+'''带参数的装饰器'''
